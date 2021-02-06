1a658bb494e3d920ff42a46bbae328e89683ce4c
EC2 Default User <ec2-user@ip-172-31-95-5.ec2.internal>
2021-02-06 05:52:21 +0000

    [Add]画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd8de48748b41fe9e0cffa34f2dae5723cb8bc70e2209b9e9fe6eb99a21ee1b7fc92fdbb8104b5b593ab3cc1439c5b2524263352ab7e517cc876214d6fc1f2bc5'
